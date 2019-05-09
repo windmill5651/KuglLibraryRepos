bf51e6a83d1c51e990b2e0408976da535a48dd23
Windmill <sobajima.ryoya.565@gmail.com>
2019-05-09 22:02:03 +0900

    first commit

---
+++ InternalAsset/GameSystem/Scene/SceneBase.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Kuglトランジションのシステム名前空間です。
+/// </summary>
+namespace Kugl.Transition.Scene
+{
+
+    /// <summary>
+    ///  SceneBase
+    ///  シーンのベース
+    ///  
+    /// Author:Windmill
+    /// </summary>
+    public abstract class SceneBase : MonoBehaviour
+    {
+
+        #region インスペクター設定フィールド
+
+        /// <summary>
+        /// シーンの設定です。
+        /// </summary>
+        [ SerializeField ]
+        private SceneSetting setting;
+
+        #endregion
+
+        
+        #region フィールド/プロパティ
+
+        /// <summary>
+        /// シーンの設定です。
+        /// </summary>
+        public SceneSetting Setting
+        {
+            get { return setting; }
+        }
+
+        #endregion
+
+
+        #region メソッド
+
+        /// <summary>
+        /// シーンをロードします。
+        /// </summary>
+        public IEnumerator LoadScene( SceneParameterBase param )
+        {
+            yield return OnLoadScene( param );
+        }
+
+        /// <summary>
+        /// シーンをオープンします。
+        /// </summary>
+        public IEnumerator OpenScene( SceneParameterBase param )
+        {
+            yield return OnOpenScene( param );
+        }
+
+        /// <summary>
+        /// シーンをクローズします。
+        /// </summary>
+        public IEnumerator CloseScene()
+        {
+            yield return OnCloseScene();
+        }
+
+        /// <summary>
+        /// シーンをロードするときの処理です。
+        /// </summary>
+        protected abstract IEnumerator OnLoadScene( SceneParameterBase param );
+
+        /// <summary>
+        /// シーンを開くときの処理です。
+        /// </summary>
+        protected abstract IEnumerator OnOpenScene( SceneParameterBase param );
+
+        /// <summary>
+        /// シーンをクローズします。
+        /// </summary>
+        /// <returns></returns>
+        protected abstract IEnumerator OnCloseScene();
+
+        #endregion
+
+    }
+
+}
---
+++ InternalAsset/UnityEngineExtension/Coroutine/ChaindCoroutine.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Unity拡張機能の名前空間です。
+/// </summary>
+namespace UnityEngine.Extensions
+{
+
+    /// <summary>
+    ///  ChainedCoroutine
+    ///  コルーチンをチェーン化するクラスです。
+    ///  
+    /// Author:Windmill
+    /// </summary>
+    public class ChaindCoroutine : IEnumerator
+    {
+        #region フィールド/プロパティ
+
+        /// <summary>
+        /// 現在のイテレータです。
+        /// </summary>
+        public object Current
+        {
+            get
+            {
+                if( current != null )
+                {
+                    return current.Current;
+                }
+
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// タスク中に発生したExceptionです。
+        /// </summary>
+        public CombinedException Exception
+        {
+            get {
+                var returnException = exceptions;
+
+                if ( ( exceptions == null ) || ( exceptions.ExceptionCount == 0 ) )
+                {
+                    return null;
+                }
+
+                return returnException;
+            }
+        }
+
+        /// <summary>
+        /// コルーチンのキューです。
+        /// </summary>
+        private Queue< SafeCoroutine > coroutineQueue = null;
+
+        /// <summary>
+        /// 現在回っているイテレータです。
+        /// </summary>
+        private IEnumerator current = null;
+
+        /// <summary>
+        /// 完了時処理です。
+        /// </summary>
+        private Action< ChaindCoroutine > onComplete = null;
+
+        /// <summary>
+        /// 実行中に発生したエラーです。
+        /// </summary>
+        private CombinedException exceptions = null;
+
+        #endregion
+
+
+        #region メソッド
+
+        /// <summary>
+        /// 空のコルーチンチェーンを返します。
+        /// </summary>
+        /// <returns>コルーチンチェーン</returns>
+        public static ChaindCoroutine Empty()
+        {
+            return new ChaindCoroutine();
+        }
+
+        /// <summary>
+        /// コンストラクタです。
+        /// </summary>
+        private ChaindCoroutine()
+        {
+            coroutineQueue = new Queue< SafeCoroutine >();
+            exceptions = new CombinedException();
+        }
+
+        /// <summary>
+        /// コルーチンを繋げます。
+        /// </summary>
+        /// <param name="task">コルーチン生成関数</param>
+        /// <returns>指定したコルーチンをつなげた自身</returns>
+        public ChaindCoroutine Continue( Func< IEnumerator > task )
+        {
+            coroutineQueue.Enqueue( new SafeCoroutine( task ) );
+            return this;
+        }
+
+        /// <summary>
+        /// 実行中の例外発生時の処理を繋げます。
+        /// </summary>
+        /// <param name="onException">例外発生時の処理</param>
+        /// <returns>指定した処理をつなげた自身</returns>
+        public ChaindCoroutine OnException( Action< Exception > onException )
+        {
+            OnComplete( ( c ) =>
+            {
+                if ( exceptions.ExceptionCount != 0 )
+                {
+                    onException( exceptions );
+                }
+
+            } );
+            return this;
+        }
+
+        /// <summary>
+        /// 実行完了時処理です。
+        /// </summary>
+        /// <param name="onComplete">コルーチンが完了した時の処理</param>
+        /// <returns>処理をつなげた自身</returns>
+        public ChaindCoroutine OnComplete( Action< ChaindCoroutine > onComplete )
+        {
+            this.onComplete += onComplete;
+            return this;
+        }
+
+        /// <summary>
+        /// イテレータを次へ進めます
+        /// </summary>
+        /// <returns>次があるかどうか</returns>
+        public bool MoveNext()
+        {
+            bool isNext = false;
+
+            // 現在のイテレータがなければキューから取得
+            if( current == null )
+            {
+                current = coroutineQueue.Dequeue();
+            }
+
+            // イテレーション
+            try
+            {
+                isNext = current.MoveNext();
+            }
+            catch( Exception e )
+            {
+                Debug.LogError( "ChainedCoroutine:" + e.Message );
+                exceptions.AddException( e );
+            }
+
+            if ( !isNext )
+            {
+                if ( coroutineQueue.Count == 0 )
+                {
+                    // キューのカウントがなくなっていたら完了
+                    if ( onComplete != null )
+                    {
+                        onComplete( this );
+                    }
+                }
+                else
+                {
+                    // まだキューが残っているなら次へ
+                    current = coroutineQueue.Dequeue();
+                    isNext = true;
+                }
+            }
+
+            return isNext;
+        }
+
+        public void Reset()
+        {
+        }
+
+        #endregion
+
+    }
+
+}
